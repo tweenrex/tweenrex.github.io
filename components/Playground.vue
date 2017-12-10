d3c0ca5215e694dcd89f2dfe40d4447ff6991924
cwallis <cwallis@wilkesmchugh.com>
2017-12-10 16:02:01 -0500

    initial iframe working

--- components/Playground.vue
+++ components/Playground.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="playground">
-        <div ref="editor" class="editor-container"></div>
-        <div class="frame-container">
-            <iframe></iframe>
+        <div class="toolbar-panes">
+            <button @click="onRun()">Run</button>
+        </div>
+        <div class="editor-panes">
+            <div ref="editor" class="editor-container"></div>
+            <div class="frame-container">
+                <iframe ref="frame"></iframe>
+            </div>
         </div>
     </div>
 </template>
@@ -13,6 +18,7 @@ const component = {
         return {
             content: 'Some content',
             editor: undefined,
+            html: '',
             isLoaded: false
         }
     },
@@ -28,14 +34,14 @@ const component = {
         if (process.browser) {
             const editorEl = self.$refs.editor;
             const defaultSlots = self.$slots.default
-            const html = ((defaultSlots.length && defaultSlots[0].text) || '').trim();
+            self.html = ((defaultSlots.length && defaultSlots[0].text) || '').trim();
 
             window.onMonacoLoad(function(monaco) {
                 self.isLoaded = true
 
                 self.editor = monaco.editor.create(editorEl, {
-                    value: html,
-                    language: 'typescript',
+                    value: self.html,
+                    language: 'javascript',
                     lineNumbers: true,
                     roundedSelection: false,
                     scrollBeyondLastLine: false,
@@ -43,6 +49,7 @@ const component = {
                     theme: 'vs-dark'
                 })
                 self.editor.layout();
+                self.onRun();
             })
         }
     },
@@ -57,8 +64,62 @@ const component = {
             if (self.editor) {
                 self.editor.layout()
             }
+        },
+        onRun() {
+            const self = this
+
+            const head = [
+                createStyle('base.css')
+            ]
+
+            const body = '<h3 class="target">Target</h3>';
+
+            const scripts = [
+                createScript('https://unpkg.com/tweenrex/dist/tweenrex-all.min.js'),
+                createScript(insertContent(body), true),
+                createScript(self.html, true)
+            ]
+
+            const html = createHTML(head, scripts)  
+
+            const frame = self.$refs.frame 
+            const doc = frame.contentWindow.document
+            doc.open();
+            doc.write(html);
+            doc.close();
         }
     }
+}
+
+function createStyle(href) {
+    return `<link rel="stylesheet" href="${href}" />`
+}
+
+function insertContent(contents) {
+    return `var el = document.createElement('div');`
+        + `el.innerHTML = '${contents}';`
+        + `document.body.appendChild(el);`
+}
+
+function onLoad(script) {
+    return `;window.addEventListener('load', function() { ` + script + `});`
+}
+
+function createScript(srcOrContents, contents) {
+    return '<script'
+        + (contents ? '' : ' src="' + srcOrContents + '"')
+        + '>'
+        + (contents ? onLoad(srcOrContents) : '')
+        + '</sc' + 'ript>'
+}
+
+function createHTML(head, js) {
+    return `
+<html>
+<head>${head.join('')}${js.join('')}</head>
+<body class="example"></body>
+</html>
+`
 }
 
 export default component
@@ -66,6 +127,12 @@ export default component
 
 <style scoped>
 .playground {
+
+}
+.toolbar-panes {
+
+}
+.editor-panes {
     max-width: 900px;
     height: 280px;
     position: relative;
@@ -81,10 +148,17 @@ export default component
 .frame-container {
     width: 50%;
     height: 280px;
+    overflow: hidden;
 }
+
 .frame-container {
     height: 100%;
     border: dashed thin black;
+}
+
+.frame-container>iframe {
+    height: 100%;
+    width: 100%;
 }
 
 .placeholder {
--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,22 @@ Insert a live playground in the middle of some markdown.
 
 <playground>
 var t1 = TweenRex({
-    duration: 1000,
+    duration: 1000, 
     subscribe: tweenrex.transform({
-        targets: '.target',
-        x: [-200, 0]
+        targets: '.target', 
+        x: [0, -6, 5, -3, 2, 0],
+        rotate: [0, -9, 7, -5, 3, 0]
+    })
+})
+t1.play()
+</playground> 
+
+<playground>
+var t1 = TweenRex({
+    duration: 1000, 
+    subscribe: tweenrex.transform({
+        targets: '.target', 
+        x: [0, -6, 5, -3, 2, 0]
     })
 })
 t1.play()
